65337f29b00d98b68789bd4425b7ce70d35783cb
violerine <viosefferine@gmail.com>
2018-05-11 11:27:59 +0700

    ujian

--- server/bin/http.js
+++ server/bin/http.js
@@ -3,6 +3,6 @@ const app = require('../app')
 
 const server = http.createServer(app)
 
-server.listen(process.env.PORT || 3000, () => {
-  console.log(`server is running on ${(process.env.PORT ? process.env.PORT : 3000)}`)
+server.listen(process.env.PORT || 5000, () => {
+  console.log(`server is running on ${(process.env.PORT ? process.env.PORT : 5000)}`)
 })
